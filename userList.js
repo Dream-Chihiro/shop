f91410d680d908ef227cdf11892464b4d83a5631
Dream-Chihiro <96460076+Dream-Chihiro@users.noreply.github.com>
2022-08-29 22:58:12 +0800

    创建用户信息列表文件

---
+++ userList.js
@@ -0,0 +1,38 @@
+let userList = [
+  { id: 1,
+    image:'https://pic.imgdb.cn/item/6305d1e616f2c2beb17bb08e.jpg',
+    head: '景浩',
+    text: '易烊千玺 饰演 演技评分:10.0',
+    relation: '景彤的哥哥'
+  },
+  { id:2,
+    image:'https://pic.imgdb.cn/item/6305d20816f2c2beb17bc854.jpg',
+    head: '梁永成',
+    text: '田雨饰演 演技评分:10.0',
+    relation: '——'
+  },
+  { id:3,
+    image: 'https://pic.imgdb.cn/item/6305d21f16f2c2beb17bd6b0.jpg',
+    head: '景彤',
+    text: '陈哈琳饰演 演技评分:10.0',
+    relation: '景浩的妹妹'
+  },
+  { 
+    id:4,image: 'https://pic.imgdb.cn/item/6305d23a16f2c2beb17be784.jpg',
+    head: '王春梅',
+    text: '齐溪饰演 演技评分:10.0',
+    relation: '——'
+  },
+  { id:5,
+    image: 'https://pic.imgdb.cn/item/6305d24d16f2c2beb17bf4c4.jpg',
+    head: '张龙豪',
+    text: '公磊饰演 演技评分:10.0',
+    relation: '——'
+  },
+  { 
+    id:6,image: 'https://pic.imgdb.cn/item/6305d27416f2c2beb17c0f65.jpg',
+    head: '张超',
+    text: '许君聪饰演 演技评分:10.0',
+    relation:'——'
+    }
+]
